5e29e297cbecc8292cba736c8b0310abdea98fbb
Alex Cohn <alexcohnb19@ufl.edu>
2018-07-12 17:49:22 -0400

    Began Working on Code
    *Created Integer Files. Ascending, Descending, and Random Order
    *Wrote the main file to read Integer files into an array to then be sorted

---
+++ Sorting Algorithm Project/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <chrono>
+#include <ctime>
+#include <ratio>
+#include "mergeSort.cpp"
+#include "selectionSort.cpp"
+using namespace std;
+int main(){
+	int size;
+	string fileName;
+	
+	cout<<"Please enter the name of the file"<<endl;
+	cin>>fileName;
+	
+	cout<<"Please enter the size of the file"<<endl;
+	cin>>size;
+	
+	int unsorted[size];
+	
+	ifstream inFile;
+	inFile.open(fileName);
+	int i = 0;
+	if(!inFile){
+		cout<<"Cant Read File";
+	}else{
+		while(inFile>>unsorted[i]){
+			i++;
+		}
+	}
+	
+	//Calculate Time
+	//Sort 1000 Times
+
+	return 0;
+}
